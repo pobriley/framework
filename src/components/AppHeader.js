625fac6f7a19987bd6c52d6150505e1601cb5bf2
pobriley <624259030@webmail.npru.ac.th>
2021-10-28 02:43:29 +0700

    first commit

---
+++ src/components/AppHeader.js
@@ -0,0 +1,11 @@
+import './AppHeader.css';
+
+function AppHeader() {
+    return (
+        <header className="app-header">
+            <h4>SIRAPOB THONGDONPHUM</h4>
+        </header>
+    );
+}
+
+export default AppHeader;
---
+++ src/data/tattoos.js
@@ -0,0 +1,10 @@
+const tattoos = [
+    { title: "ปู่จอมซ่าบ้าพลัง", thumbnailUrl: "/images/pixel-01-small.jpg", fullUrl: "/images/pixel-01-large.jpg" },
+    { title: "หนูน้อยซ่าบ้าพลัง", thumbnailUrl: "/images/pixel-02-small.jpg", fullUrl: "/images/pixel-02-large.jpg" },
+    { title: "โกงซ่าบ้าพลัง", thumbnailUrl: "/images/pixel-03-small.jpg", fullUrl: "/images/pixel-03-large.jpg" },
+    { title: "ดูก", thumbnailUrl: "/images/pixel-04-small.jpg", fullUrl: "/images/pixel-04-large.jpg" },
+    { title: "ลูกน้องหมาโหด", thumbnailUrl: "/images/pixel-05-small.jpg", fullUrl: "/images/pixel-05-large.jpg" },
+    { title: "หัวหน้าหมาโหด", thumbnailUrl: "/images/pixel-06-small.jpg", fullUrl: "/images/pixel-06-large.jpg" },
+];
+
+export default tattoos;
